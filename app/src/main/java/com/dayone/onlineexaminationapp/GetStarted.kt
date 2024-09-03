4ed9dfd5cddc0cc2e584b7a1e07c589978c780bd
Pixel Pulse <aquinosherwinjosh@gmail.com>
2024-09-03 13:55:02 +0800

    i updated the getstarted button into login

--- app/src/main/java/com/dayone/onlineexaminationapp/GetStarted.kt
+++ app/src/main/java/com/dayone/onlineexaminationapp/GetStarted.kt
@@ -1,6 +1,8 @@
 package com.dayone.onlineexaminationapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,23 @@ class GetStarted : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.get_started)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+
+        val getstartedbutton = findViewById<Button>(R.id.btngetstarted)
+
+        getstartedbutton.setOnClickListener {
+            val intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
         }
+
+
+
+
+
+
+
+
+
     }
 }
