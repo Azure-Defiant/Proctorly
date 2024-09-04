b82167e558d46520bc3d0f390fc7ab9e36f7ac86
Pixel Pulse <aquinosherwinjosh@gmail.com>
2024-09-04 18:06:20 +0800

    added sign in and added password toggle

--- app/src/main/java/com/dayone/onlineexaminationapp/SignIn.kt
+++ app/src/main/java/com/dayone/onlineexaminationapp/SignIn.kt
@@ -1,29 +1,20 @@
 package com.dayone.onlineexaminationapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LogIn : AppCompatActivity() {
+class SignIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login)
-
-        val signinbutton = findViewById<Button>(R.id.signinbtn)
-
-        signinbutton.setOnClickListener {
-            val intent = Intent(this, SignIn::class.java)
-            startActivity(intent)
+        setContentView(R.layout.activity_sign_in)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-
-
-
-
-
     }
 }
